8e50e4a3330c982f0e1ce96a0862fd7bf5e44c7d
vivekratr <vivekratr@gmail.com>
2023-12-16 14:56:44 +0530

    Add NSFW image classification functionality

--- src/component/Getstarted.jsx
+++ src/component/Getstarted.jsx
@@ -10,6 +10,16 @@ import { FunctionComponent } from "react";
 
 const Getstarted: FunctionComponent = () => {
   return (
+
+
+    <div className="relative w-full h-5 text-left text-[16px] text-white font-inter">
+      <div className="absolute top-[1px] left-[0px] font-medium whitespace-pre-wrap">{`Made With        in Mumbai Hacks `}</div>
+      <img
+        className="absolute top-[0px] left-[89px] w-5 h-5 overflow-hidden object-cover"
+        alt=""
+        src="/mdiheart@2x.png"
+      />
+    </div>
     <div className="relative bg-black box-border w-full h-[800px] overflow-hidden text-left text-[16px] text-white font-inter border-[1px] border-solid border-white">
       <div className="absolute top-[600px] left-[780px] rounded-[44px] [background:linear-gradient(180.13deg,_#202020,_#181818)] box-border w-[132px] h-11 overflow-hidden border-t-[2px] border-solid border-gray border-r-[1px] border-l-[1px]">
         <div className="absolute top-[13px] left-[21px] font-medium">
@@ -17,13 +27,24 @@ const Getstarted: FunctionComponent = () => {
         </div>
       </div>
       <img
-        className="absolute top-[34px] left-[1600px] rounded-lg w-[45px] h-[45px] overflow-hidden object-cover"
+        className="absolute top-[34px] left-[1600px] w-[50px] h-[50px] overflow-hidden object-cover"
         alt=""
         src="https://cdn.discordapp.com/attachments/1184864067295395960/1185499752985526352/image.png?ex=658fd5ae&is=657d60ae&hm=80fb47295c2221ca3040c87eaa89a559fcb9ea3d802e3022f6d9b22f00c1a9d2&"
       />
       <b className="absolute top-[450px] left-[700px] text-[23px] ">
         Decentralized Connectivity
       </b>
+
+      <div className="relative top-[600px] left-[780px] w-full h-11 text-left text-[16px] text-white font-inter">
+        <img
+          className="absolute top-0 border-transparent left-0 w-132 h-11 object-cover border"
+          alt=""
+          src="https://cdn.discordapp.com/attachments/1184864067295395960/1185503296924946452/image.png?ex=658fd8fb&is=657d63fb&hm=9ed3c8500b74d1698dbb8535dd329ef0771e9d1fc7fcb02ed0d35332a9716a86&"
+        />
+        <div className="absolute top-[13px] left-[21px] font-medium">
+          Get Started
+        </div>
+      </div>
     </div>
   );
 };
--- src/component/NSFW.jsx
+++ src/component/NSFW.jsx
@@ -1,22 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 const classifyImage = async (imageUrl) => {
   try {
-    const response = await axios.post('https://nsfw-detect3.p.rapidapi.com/nsfw-detect', {
+    const data = JSON.stringify({
       url: imageUrl,
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-RapidAPI-Key': '5e00906437msh0bfd27ae14a8d01p1ab669jsnfb05e107a87d',
-        'X-RapidAPI-Host': 'nsfw-detect3.p.rapidapi.com',
-      },
     });
 
-    console.log(response.data);
-    // Handle the NSFW classification result here
-    // You can update the component state or take any other action based on the result
-    return response.data; // Return the response data
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener('readystatechange', function () {
+      if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+        // Handle the NSFW classification result here
+        // You can update the component state or take any other action based on the result
+      }
+    });
+
+    xhr.open('POST', 'https://nsfw-images-detection-and-classification.p.rapidapi.com/adult-content');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.setRequestHeader('X-RapidAPI-Key', '5e00906437msh0bfd27ae14a8d01p1ab669jsnfb05e107a87d');
+    xhr.setRequestHeader('X-RapidAPI-Host', 'nsfw-images-detection-and-classification.p.rapidapi.com');
+
+    xhr.send(data);
   } catch (error) {
     console.error('Error classifying image:', error);
     return null; // Return null in case of an error
@@ -28,11 +34,12 @@ const NSFW = () => {
 
   useEffect(() => {
     // Test image classification from URL
-    const imageUrl = 'https://cdn.discordapp.com/attachments/858938620425404426/1076560199218892902/waifu-animemoeus.jpg';
+    const imageUrl = 'https://media.istockphoto.com/id/185305538/photo/killing-scene.jpg?s=1024x1024&w=is&k=20&c=FBqywOpGHibrTNZ6NbRgd_6RRn5dQfY4MXjamCfCEg4=';
 
     const fetchData = async () => {
-      const result = await classifyImage(imageUrl);
-      setClassificationResult(result);
+      await classifyImage(imageUrl);
+      // Set the classification result if needed
+      // setClassificationResult(result);
     };
 
     fetchData();
