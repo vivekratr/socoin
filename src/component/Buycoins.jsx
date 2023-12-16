95fb4f973c8f9631ca517193b81df9c963d1f568
vivekratr <vivekratr@gmail.com>
2023-12-16 23:30:13 +0530

    Refactor Buycoins component layout

--- src/component/Buycoins.jsx
+++ src/component/Buycoins.jsx
@@ -1,38 +1,46 @@
 import React from "react";
 
+
+
 const Buycoins = () => {
   return (
-    <div className="relative rounded-2xl [background:linear-gradient(180.13deg,_#202020,_#181818)] box-border w-full h-[229px] overflow-hidden text-left text-sm text-black font-inter border-t-[2px] border-solid border-gray border-r-[1px] border-l-[1px]">
-      <img
-        className="absolute top-[13px] left-[50px] w-[128.45px] h-[135.89px] object-cover"
-        alt=""
-        src="/3-d-glassmorphism-crypto-initial-coin-offerings-announcement-instagram-post-1@2x.png"
-      />
-      <img
-        className="absolute top-[10px] left-[60px] w-[174.96px] h-[178.22px] object-cover"
-        alt=""
-        src="/3-d-glassmorphism-crypto-initial-coin-offerings-announcement-instagram-post-2@2x.png"
-      />
-      <div className="absolute top-[174px] left-[147px] rounded-md [background:linear-gradient(106.75deg,_#fdd835,_#fff_49.15%,_#ffd000)] box-border w-[103px] h-[41px] overflow-hidden border-t-[1px] border-solid border-cornsilk border-r-[1px] border-l-[1px]">
-        <div className="absolute top-[12px] left-[18px] font-medium">
-          Buy Coins
+    <div className="relative w-full h-[229px] text-left text-sm text-black font-inter">
+      <div className="absolute top-[0px] left-[0px] rounded-2xl [background:linear-gradient(180.13deg,_#202020,_#181818)] box-border w-[271px] h-[229px] overflow-hidden border-t-[2px] border-solid border-gray border-r-[1px] border-l-[1px]">
+        <img
+          className="absolute top-[13px] left-[50px] w-[128.45px] h-[135.89px] object-cover"
+          alt=""
+          src="   "
+        />
+        <img
+          className="absolute top-[10px] left-[60px] w-[174.96px] h-[178.22px] object-cover"
+          alt=""
+          src="https://cdn.discordapp.com/attachments/1184864067295395960/1185641928847527936/image.png?ex=65905a17&is=657de517&hm=652d84dd8d1a39a6d6ea2850e469a17fb054c4a708f566aab57bc6428afdcb0e&"
+        />
+        <div className="absolute top-[174px] left-[147px] rounded-md [background:linear-gradient(106.75deg,_#fdd835,_#fff_49.15%,_#ffd000)] box-border w-[103px] h-[41px] overflow-hidden border-t-[1px] border-solid border-cornsilk border-r-[1px] border-l-[1px]">
+          <div className="absolute top-[12px] left-[18px] font-medium">
+            Buy Coins
+          </div>
         </div>
-      </div>
-      <div className="absolute top-[174px] left-[24px] rounded-md bg-white box-border w-[103px] h-[41px] overflow-hidden border-t-[1px] border-solid border-gainsboro border-r-[1px] border-l-[1px]">
-        <div className="absolute top-[12px] left-[37px] font-semibold">100</div>
-        <img
-          className="absolute top-[11px] left-[70px] w-[19px] h-[19px] overflow-hidden object-cover"
-          alt=""
-          src="/majesticonsplusline@2x.png"
-        />
-        <img
-          className="absolute top-[12px] left-[10px] w-[19px] h-[18px] overflow-hidden object-cover"
-          alt=""
-          src="/majesticonsplusline@2x.png"
-        />
+        <div className="absolute top-[174px] left-[24px] rounded-md bg-white box-border w-[103px] h-[41px] overflow-hidden border-t-[1px] border-solid border-gainsboro border-r-[1px] border-l-[1px]">
+          <div className="absolute top-[12px] left-[37px] font-semibold">
+            100
+          </div>
+          <img
+            className="absolute top-[11px] left-[70px] w-[19px] h-[19px] overflow-hidden object-cover"
+            alt=""
+            src="/majesticonsplusline@2x.png"
+          />
+          <img
+            className="absolute top-[12px] left-[10px] w-[19px] h-[18px] overflow-hidden object-cover"
+            alt=""
+            src="/majesticonsplusline@2x.png"
+          />
+        </div>
       </div>
     </div>
   );
 };
+
+
 
 export default Buycoins;
